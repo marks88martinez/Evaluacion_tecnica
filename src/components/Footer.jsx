44ca26c4572e6a50eaa491115feb3f947b92daa8
marcos <marks88martinez@gmail.com>
2024-05-04 17:00:27 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="bg-blue-950 text-white p-4 mt-4">
-    <div className="container mx-auto text-center">
-      <p>© 2024 Mi Sitio Web. Todos los derechos reservados.</p>
-    </div>
-  </footer>
+
+<footer className="bg-blue-950 text-blue-100 p-4 mt-4">
+  <div className="container mx-auto text-center">
+    <p>Realizado por Marcos Martinez | <a href="https://www.linkedin.com/in/marksmartinez/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}><strong>LinkedIn</strong></a></p>
+    <p>© 2024 Mi Sitio Web. Todos los derechos reservados.</p>
+  </div>
+</footer>
+
   
   )
 }
