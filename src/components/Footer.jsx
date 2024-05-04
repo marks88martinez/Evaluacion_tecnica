1f65542f27ab338be5b8da5c6fed68ba0562e41d
marcos <marks88martinez@gmail.com>
2024-05-04 17:03:11 -0400

    modificaciones en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
 
 <footer className="bg-blue-950 text-blue-100 p-4 mt-4">
   <div className="container mx-auto text-center">
-    <p>Realizado por Marcos Martinez | <a href="https://www.linkedin.com/in/marksmartinez/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}><strong>LinkedIn</strong></a></p>
+    <p>Realizado por <a href="https://www.linkedin.com/in/marksmartinez/" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>Marcos Martinez | <strong>LinkedIn</strong></a></p>
     <p>© 2024 Mi Sitio Web. Todos los derechos reservados.</p>
   </div>
 </footer>
